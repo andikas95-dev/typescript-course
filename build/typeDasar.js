87c9da447c853de7fb6676f3257378a62b970997
andikas95-dev <andikaperdana95@gmail.com>
2020-08-01 03:49:39 +0700

    type data typescript dan penulisannya

---
+++ build/typeDasar.js
@@ -0,0 +1,17 @@
+"use strict";
+// type deklarasi penulisan typescript
+var contoh1 = "test";
+var contoh = "test";
+var contoh3;
+contoh3 = "test";
+// type Data String
+var nama = "Andika";
+var buah = "pisang";
+// akan error ketika diisi selain string
+// type Data Number
+var angka = 20;
+//type Data Boolean
+var isMarried = true;
+//type Data Any = Type data yang isinya bisa diisi apapun, dari string bisa jadi angka dll
+var mancing = "ayok";
+mancing = 50000;
---
+++ src/typeDasar.ts
@@ -0,0 +1,20 @@
+// type deklarasi penulisan typescript
+let contoh1 = "test";
+let contoh: string = "test";
+let contoh3: string;
+contoh3 = "test";
+
+// type Data String
+let nama = "Andika";
+let buah: string = "pisang";
+// akan error ketika diisi selain string
+
+// type Data Number
+let angka: number = 20;
+
+//type Data Boolean
+let isMarried: boolean = true;
+
+//type Data Any = Type data yang isinya bisa diisi apapun, dari string bisa jadi angka dll
+let mancing: any = "ayok";
+mancing = 50000;
